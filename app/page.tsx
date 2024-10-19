dfef3b77cff7b682c0e05999b7be2191736f3e6e
younguk <0622ook@gmail.com>
2024-10-19 10:33:17 +0900

    'first'

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,44 @@
 import Image from "next/image";
+import younguk from '../img.jpg';
+import { SignIn, SignedIn } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div style={{ backgroundColor: '#f6f6f6' }}>
+    <div className="ml-20 mr-20">
+      <h3 className="font-bold text-3xl pb-3 pt-6">About me</h3>
+      <Image src={younguk} alt="younguk" height={400} className="ml-5 mr-10 border-gray-500 border-4 mt-6 left-0 float-left" />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="border-gray-400 border-2  mt-6 bg-white inline-block" style={{ borderRadius: '20px' }}>
+        {/* 출처: https://nextjs.org/docs/pages/api-reference/components/image */}
+
+
+        <div className="p-7 pr-44">
+          <p className="text-lg">이름: 김영욱</p>
+          <p className="text-lg">생년월일: 2003/11/12</p>
+          <p className="text-lg">거주지역: 검단 오류동(인천)</p>
+          <p className="text-lg">연락처: 010-4487-0622</p>
+          <p className="text-lg">학교: 중부대학교</p>
+          <p className="text-lg">학번: 92212788</p>
+          <p className="text-lg">학과: 정보보호학전공</p>
+          <p className="text-lg">학년: 2학년</p>
+          <p className="text-lg">종교: 기독교(개신교)</p>
+          <p className="text-lg">MBTI: INFT-J</p>
+          <p className="text-lg">취미: 기타,그림,축구영상보기</p>
+          <p className="text-lg">좋아하는 분야: 프로그래밍(JAVA, Python 등)</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+    </div>
+
+    {/* <SignedIn>
+      <div>
+        글쓰기
+        <div>
+          <input type="text" />
+        </div>
+      </div>
+      </SignedIn> */}
+    
     </div>
   );
 }
